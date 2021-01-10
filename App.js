152f05dcb7bd81fa0c4d424fde046b275e4de347
MatiRomero <mgr_89@hotmail.com>
2021-01-10 12:22:59 -0300

    header

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import Header from './Components/Header';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
