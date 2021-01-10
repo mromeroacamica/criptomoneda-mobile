6e979378d07ea894c3c51f7d930f0c81c807f168
MatiRomero <mgr_89@hotmail.com>
2021-01-10 12:58:22 -0300

    label select

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {StyleSheet, Image, View} from 'react-native';
 import Header from './Components/Header';
+import Formulario from './Components/Formulario';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Image
+        style={styles.image}
+        source={require('./assets/assets/img/cryptomonedas.png')}
+      />
+      <View style={styles.contenido}>
+        <Formulario />
+      </View>
     </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 150,
+    marginHorizontal: '2.5%',
+  },
+  contenido: {
+    marginHorizontal: '2.5%',
+  },
+});
 
 export default App;
---
+++ Components/Formulario.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const Formulario = () => {
+  return (
+    <View>
+      <Text style={styles.label}>Moneda</Text>
+      <Text style={styles.label}>Cryptomoneda</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  label: {
+    fontFamily: 'Lato-Black',
+    textTransform: 'uppercase',
+    fontSize: 22,
+    marginVertical: 20,
+  },
+});
+
+export default Formulario;
