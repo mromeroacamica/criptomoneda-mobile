59a7cfb068047a3b2120774c68db775d82c813e2
MatiRomero <mgr_89@hotmail.com>
2021-01-30 11:32:36 -0300

    app terminada

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, Image, View} from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  View,
+  ScrollView,
+  ActivityIndicator,
+} from 'react-native';
 import Header from './Components/Header';
 import Formulario from './Components/Formulario';
 import Cotizacion from './Components/Cotizacion';
@@ -10,37 +16,52 @@ const App = () => {
   const [criptoMoneda, guardarCriptoMoneda] = useState('');
   const [consultarApi, guardarConsultarApi] = useState(false);
   const [resultado, guardarResultado] = useState({});
+  const [cargando, guardarCargando] = useState(false);
 
   useEffect(() => {
     const cotizarCriptomoneda = async () => {
       if (consultarApi) {
         const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptoMoneda}&tsyms=${moneda}`;
-        console.log(url);
         const resultado = await axios.get(url);
-        guardarResultado(resultado.data.DISPLAY[criptoMoneda][moneda]);
-        guardarConsultarApi(false);
+        guardarCargando(true);
+        //Ocultar el spinner y mostrar el resultado
+        setTimeout(() => {
+          guardarResultado(resultado.data.DISPLAY[criptoMoneda][moneda]);
+          guardarConsultarApi(false);
+          guardarCargando(false);
+        }, 3000);
       }
     };
     cotizarCriptomoneda();
   }, [consultarApi]);
 
+  //mostrar el spinner o el resultado
+
+  const componente = cargando ? (
+    <ActivityIndicator size="large" color="#5e49e2" />
+  ) : (
+    <Cotizacion resultado={resultado} />
+  );
+
   return (
     <>
-      <Header />
-      <Image
-        style={styles.image}
-        source={require('./assets/assets/img/cryptomonedas.png')}
-      />
-      <View style={styles.contenido}>
-        <Formulario
-          moneda={moneda}
-          criptoMoneda={criptoMoneda}
-          guardarCriptoMoneda={guardarCriptoMoneda}
-          guardarMoneda={guardarMoneda}
-          guardarConsultarApi={guardarConsultarApi}
+      <ScrollView>
+        <Header />
+        <Image
+          style={styles.image}
+          source={require('./assets/assets/img/cryptomonedas.png')}
         />
-        <Cotizacion resultado={resultado} />
-      </View>
+        <View style={styles.contenido}>
+          <Formulario
+            moneda={moneda}
+            criptoMoneda={criptoMoneda}
+            guardarCriptoMoneda={guardarCriptoMoneda}
+            guardarMoneda={guardarMoneda}
+            guardarConsultarApi={guardarConsultarApi}
+          />
+        </View>
+        <View style={{marginTop: 40}}>{componente}</View>
+      </ScrollView>
     </>
   );
 };
--- Components/Cotizacion.js
+++ Components/Cotizacion.js
@@ -3,7 +3,47 @@ import {StyleSheet, Text, View} from 'react-native';
 
 const Cotizacion = ({resultado}) => {
   if (Object.keys(resultado).length === 0) return null;
-  return <Text>Este es el precio de cripto{resultado.PRICE}</Text>;
+  return (
+    <View style={styles.resultado}>
+      <Text style={[styles.texto, styles.precio]}>
+        <Text style={styles.span}>{resultado.PRICE}</Text>
+      </Text>
+      <Text style={styles.texto}>
+        Precio más alto del día: {''}
+        <Text style={styles.span}>{resultado.HIGHDAY}</Text>
+      </Text>
+      <Text style={styles.texto}>
+        Precio más bajo del día: {''}
+        <Text style={styles.span}>{resultado.LOWDAY}</Text>
+      </Text>
+      <Text style={styles.texto}>
+        Variación últimas 24 horas: {''}
+        <Text style={styles.span}>{resultado.CHANGEPCT24HOUR} %</Text>
+      </Text>
+      <Text style={styles.texto}>
+        Última actualización: {''}
+        <Text style={styles.span}>{resultado.LASTUPDATE}</Text>
+      </Text>
+    </View>
+  );
 };
+const styles = StyleSheet.create({
+  resultado: {
+    backgroundColor: '#5e49e2',
+    padding: 20,
+  },
+  texto: {
+    color: '#fff',
+    fontFamily: 'Lato-Regular',
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  precio: {
+    fontSize: 38,
+  },
+  span: {
+    fontFamily: 'Lato-Black',
+  },
+});
 
 export default Cotizacion;
